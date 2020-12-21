fb1aba69b1314084eac12832a3a25e1436ef5bf0
Masayoshi Mizutani <mizutani@cookpad.com>
2020-12-21 11:39:16 +0900

    Remove .d.ts and .js generated by tsc

--- example/deployable/cdk/test-stack.d.ts
+++
@@ -1,5 +0,0 @@
-import "source-map-support/register";
-import * as cdk from '@aws-cdk/core';
-export declare class GolambdaTestStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps);
-}
